2bd6fbce8a34b07895e1b1e730abcb9f659b5307
a <bbbbb>
2018-03-15 10:37:34 +0800

    首次上传代码

---
+++ src/main/java/com/atguigu/maven/Test.java
@@ -0,0 +1,10 @@
+package com.atguigu.maven;
+
+import com.atguigu.maven.entities.Book;
+
+public class Test {
+	public static void main(String[] args) {
+		Book book = new Book(1, "aa", 33.3);
+		book.setName("bbbb");
+	}
+}
